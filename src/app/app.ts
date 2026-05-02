835d6fd0b7ea2ba57dc6d6b2d91e7354e66e62b4
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2026-05-02 19:17:31 +0200

    second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ MatToolbarModule, MatSidenavModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('simple-crm');
